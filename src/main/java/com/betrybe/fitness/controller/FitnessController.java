8fab9265786d125481293318575cd5e9be26414f
Fellipe Marcel <fmrclms@gmail.com>
2023-12-05 14:10:45 -0300

    feat: implements GET /fitness/workouts/{id} route

--- src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -1,8 +1,12 @@
 package com.betrybe.fitness.controller;
 
+import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.service.FitnessServiceInterface;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +27,19 @@ public class FitnessController implements FitnessControllerInterface {
   @GetMapping
   public String getRoot() {
     return "Boas vindas à API de Fitness!";
+  }
+
+  /**
+   * Gets workout.
+   *
+   * @param id the id
+   * @return the workout
+   */
+  @GetMapping("/workouts/{id}")
+  public ResponseEntity<WorkoutDto> getWorkout(@PathVariable Long id) {
+    Optional<WorkoutDto> workout = fitnessService.getWorkout(id);
+
+    return workout.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
   }
 }
