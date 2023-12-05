426be037e4f1cc198e88785aa82cbc6714f504ce
Fellipe Marcel <fmrclms@gmail.com>
2023-12-05 14:35:40 -0300

    feat: creates POST /fitness/workouts route

--- src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -1,5 +1,6 @@
 package com.betrybe.fitness.controller;
 
+import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.service.FitnessServiceInterface;
 import java.util.Optional;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +44,16 @@ public class FitnessController implements FitnessControllerInterface {
 
     return workout.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 
+  }
+
+  /**
+   * Save workout response entity.
+   *
+   * @param workoutDto the workout dto
+   * @return the response entity
+   */
+  @PostMapping("/workouts")
+  public ResponseEntity<WorkoutDto> saveWorkout(@RequestBody WorkoutCreationDto workoutDto) {
+    return ResponseEntity.status(201).body(fitnessService.saveWorkout(workoutDto));
   }
 }
