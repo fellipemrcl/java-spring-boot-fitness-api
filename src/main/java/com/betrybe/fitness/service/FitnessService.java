0274cb7f82434757ae672d40ef44feabfdd1691c
Fellipe Marcel <fmrclms@gmail.com>
2023-12-05 10:51:58 -0300

    feat: implements getWorkout in FitnessService class

--- src/main/java/com/betrybe/fitness/service/FitnessService.java
+++ src/main/java/com/betrybe/fitness/service/FitnessService.java
@@ -3,6 +3,7 @@ package com.betrybe.fitness.service;
 import com.betrybe.fitness.database.FakeFitnessDatabase;
 import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
+import com.betrybe.fitness.model.Workout;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class FitnessService implements FitnessServiceInterface {
 
+  FakeFitnessDatabase database;
+
   @Autowired
   public FitnessService(FakeFitnessDatabase database) {
+    this.database = database;
   }
 
   @Override
@@ -25,7 +29,21 @@ public class FitnessService implements FitnessServiceInterface {
 
   @Override
   public Optional<WorkoutDto> getWorkout(Long id) {
-    return Optional.empty();
+    Optional<Workout> optWorkout = database.getWorkout(id);
+
+    if (optWorkout.isEmpty()) {
+      return Optional.empty();
+    }
+
+    Workout workout = optWorkout.get();
+
+    WorkoutDto workoutDto = new WorkoutDto(
+        workout.getId(),
+        workout.getName(),
+        workout.getRepetitions()
+    );
+
+    return Optional.of(workoutDto);
   }
 
   @Override
